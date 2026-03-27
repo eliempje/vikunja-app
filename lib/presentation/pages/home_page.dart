c3f8e8103f68eb3a13e6e9a44a72addf2076e92d
eliempje <emiel__@hotmail.com>
2026-03-28 00:42:19 +0100

    fix: streamline notification permission check in HomePageState

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -234,8 +234,7 @@ class HomePageState extends ConsumerState<HomePage> {
   }
 
   Future<void> initNotifications() async {
-    var notifGranted = await Permission.notification.
-    isGranted;
+    var notifGranted = await Permission.notification.isGranted;
     if (notifGranted) {
       NotificationHandler notificationHandler = NotificationHandler();
       await notificationHandler.initNotifications();
